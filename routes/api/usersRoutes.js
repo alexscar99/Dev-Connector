a3feae080e41928e678f146bb55e53b2b1c1a593
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-24 00:18:22 -0400

    added login post route, sends back jwt on correct login

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 const User = require('../../models/User');
 
@@ -37,6 +39,39 @@ router.post('/register', (req, res) => {
         });
       });
     }
+  });
+});
+
+// Login user
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email }).then(user => {
+    // check user
+    if (!user) return res.status(404).json({ email: 'User not found' });
+
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // JWT payload
+        const { _id, name, avatar } = user;
+        const payload = { _id, name, avatar };
+
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
   });
 });
 
