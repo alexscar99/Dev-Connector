cabd498ba80dcbfbfce5ab0f93d89a8d712c8076
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-25 03:34:40 -0400

    created validation for education and experience

---
+++ validation/education.js
@@ -0,0 +1,33 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateEducationInput(data) {
+  let errors = {};
+  let { school, degree, fieldofstudy, from } = data;
+
+  school = !isEmpty(school) ? school : '';
+  degree = !isEmpty(degree) ? degree : '';
+  fieldofstudy = !isEmpty(fieldofstudy) ? fieldofstudy : '';
+  from = !isEmpty(from) ? from : '';
+
+  if (Validator.isEmpty(school)) {
+    errors.school = 'School field is required';
+  }
+
+  if (Validator.isEmpty(degree)) {
+    errors.degree = 'Degree field is required';
+  }
+
+  if (Validator.isEmpty(fieldofstudy)) {
+    errors.fieldofstudy = 'Field of study is required';
+  }
+
+  if (Validator.isEmpty(from)) {
+    errors.from = 'From date field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
