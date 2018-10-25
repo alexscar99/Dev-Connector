3e32d58f9b399faff45fbc354a50f49e3a1eec80
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-25 03:34:57 -0400

    created model for profile

--- models/Profile.js
+++ models/Profile.js
@@ -116,6 +116,6 @@ const ProfileSchema = new Schema({
   }
 });
 
-const Profile = mongoose.model('profiles', ProfileSchema);
+const Profile = mongoose.model('profile', ProfileSchema);
 
 module.exports = Profile;
