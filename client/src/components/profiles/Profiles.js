aaed037807eddeabdce26cd1a195b084e721e8b7
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-29 04:08:19 -0400

    fixed key prop for ProfileItem components being returned from mapping over profiles, filled ProfileHeader with dynamic content

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -21,7 +21,7 @@ class Profiles extends Component {
     } else {
       if (profiles.length > 0) {
         profileItems = profiles.map(profile => (
-          <ProfileItem key={profile.id} profile={profile} />
+          <ProfileItem key={profile._id} profile={profile} />
         ));
       } else {
         profileItems = <h4>No Profiles Found</h4>;
