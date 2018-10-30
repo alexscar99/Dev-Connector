457659c10302b263fb5fc7268e28ca6042cd4851
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-30 16:53:46 -0400

    Action for getting all posts, set up component for the posts feed and added conditional to determine what to render as the post content

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { ADD_POST, GET_POST, GET_ERRORS } from './types';
+import { ADD_POST, GET_POSTS, GET_ERRORS, POST_LOADING } from './types';
 
 // add post
 export const addPost = postData => async dispatch => {
@@ -11,4 +11,21 @@ export const addPost = postData => async dispatch => {
   } catch (err) {
     dispatch({ type: GET_ERRORS, payload: err.response.data });
   }
+};
+
+// get all posts
+export const getPosts = () => async dispatch => {
+  dispatch(setPostLoading());
+  try {
+    const res = await axios.get('/api/posts');
+
+    dispatch({ type: GET_POSTS, payload: res.data });
+  } catch (err) {
+    dispatch({ type: GET_POSTS, payload: null });
+  }
+};
+
+// handle profile loading
+export const setPostLoading = () => {
+  return { type: POST_LOADING };
 };
